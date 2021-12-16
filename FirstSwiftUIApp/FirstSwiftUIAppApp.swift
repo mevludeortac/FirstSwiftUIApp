ff505e23a313bd69c4305d9c5742d64eb6b9f154
goldbilgisayar <ortacmevlude@gmail.com>
2021-12-16 20:27:27 +0300

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by MEWO on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
